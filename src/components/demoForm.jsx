81fb0e3e6ac15b9c2aba4583876a06719a34e358
Fenil <fenilshah23398@gmail.com>
2019-06-05 15:28:35 +0530

    Validations done

--- src/components/demoForm.jsx
+++ src/components/demoForm.jsx
@@ -1,9 +1,52 @@
-import React, { Component } from 'react';
-class demoForm extends Component {
-    state = {  }
-    render() { 
-        return <h1>DemoForm </h1>;
-    }
-}
- 
+import React, { Component } from "react";
+import { Formik, Form, Field, ErrorMessage } from "formik";
+import * as Yup from "yup";
+
+const validateFirst = Yup.object().shape({
+  name: Yup.string()
+    .required("Required Field")
+    .min(3, "Atleast 3 characters")
+    .max(10, "Dont got much dn Limit"),
+
+  amail: Yup.string()
+    .required("Required")
+    .email("Invalid email"),
+
+  mobile: Yup.number("Enter Number Values")  //if i want to match length than i have to do this code manually using function
+            .typeError("Only Numbers")      //as min max minlength works with its value
+            .required("requires")
+});
+const demoForm = () => (
+  <div>
+    <h1>DemoFoem Validations</h1>
+    <Formik
+      initialValues={{ name: "", 
+      amail: "",
+    mobile:"" }}
+      validationSchema={validateFirst}
+      onSubmit={values => {
+        alert("came");
+        console.log(values);
+      }}
+    >
+      {({ errors, touched }) => (
+        <Form>
+          <Field name="amail" placeholder="Enter Email" />
+          <ErrorMessage name="amail" /><br/>
+
+          <Field name="name" placeholder="Enter Name"/>
+          {errors.name && touched.name ? <div>{errors.name}</div> : null}
+          <ErrorMessage name="name" /><br/>
+
+            <Field name="mobile" placeholder="Enter mobile"/>
+            <ErrorMessage name="mobile" />
+
+          <button type="submit">Submit</button>
+        </Form>
+      )}
+    </Formik>
+  </div>
+);
+
 export default demoForm;
+// export default demoForm;
